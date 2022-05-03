4ae13079b6ea74d7819c6935db901d2e561c3b60
Daniel Reis <dreis@opensourceintegrators.com>
2022-05-03 09:08:22 +0100

    [IMP] stock_picking_info_lot: require lot info upon validation

--- stock_picking_info_lot/models/__init__.py
+++ stock_picking_info_lot/models/__init__.py
@@ -1,1 +1,3 @@
-from . import product_template, stock_move_line
+from . import product_template
+from . import stock_move_line
+from . import stock_picking
--- stock_picking_info_lot/models/stock_move_line.py
+++ stock_picking_info_lot/models/stock_move_line.py
@@ -4,8 +4,7 @@ from odoo import fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    lot_info = fields.Char("Lot/Serial Number info")
+    lot_info = fields.Char("Lot/Serial Number Info")
     lot_info_usage = fields.Selection(
-        [("no", "No"), ("optional", "Optional"), ("required", "Required")],
-        related="product_id.product_tmpl_id.lot_info_usage",
+        related="product_id.product_tmpl_id.lot_info_usage"
     )
