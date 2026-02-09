b65ca9c46c7dee98ee14a94084d88595dd6011bb
Daniel Reis <dreis@opensourceintegrators.com>
2026-02-09 18:07:50 +0000

    Revert "[ADD] stock_lock_lot_stage: Lot stages for quality control workflows"
    This reverts commit 03eebcd885fa2e57d97eeca3438d50eebba3244f.

--- stock_lock_lot_stage/readme/CONTRIBUTORS.md
+++
@@ -1,2 +0,0 @@
-- [Open Source Integrators](https://www.opensourceintegrators.com):
-  - Daniel Reis <dreis@opensourceintegrators.com>
--- stock_lock_lot_stage/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2025 Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import test_stock_lock_lot_stage
